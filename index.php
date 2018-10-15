d86e7b7783baa0ec5310ffe86befe7d39c461cb4
sishen520 <34979795+sishen520@users.noreply.github.com>
2018-10-15 22:27:55 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,178 @@
+<!doctype html>
+<html>
+<head>
+    <meta name="baidu-site-verification" content="XDk93xXVav" />
+<meta charset="gb2312">
+<title>魏晴べ青涩の初夏</title>
+<meta name="Keywords" content="魏晴，小2小2小小巫" >
+<meta name="Description" content="魏晴" >
+<link href="css/index.css" rel="stylesheet">
+<link rel="shortcut icon" href="images/tubiao.ico">
+<!--[if lt IE 9]>
+<script src="js/modernizr.js"></script>
+<![endif]-->
+</head>
+<body>
+<header>
+  <h1><a href="/">青涩の初夏</a></h1>
+  <p>趁我们都还年轻,多欣赏下沿途的风景，不要错过了流年里温暖的人和物....</p>
+</header>
+<!--nav begin-->
+<div id="nav">
+  <ul>
+    <li><a href="/">首页</a></li>
+    <li><a href="jilu.php">生活记录</a></li>
+    <li><a href="photo.php">相册</a></li>
+    <li><a href="about.php">关于我</a></li>
+    <li><a href="messageBoard.php">留言板</a></li>
+    <li><a href="yinyue.php" target="_blank">音乐欣赏</a></li>
+  </ul>
+</div>
+<!--nav end-->
+<div class="blank"></div>
+<div class="banner">
+  <ul class="boy_girl">
+    <li class="girlimg"><a href="about-2.php"><span>关于她</span></a></li>
+    <li class="boyimg"><a href="about.php"><span>关于我</span></a></li>
+  </ul>
+  <ul class="texts">
+    <p><img src="images/t-1.png" alt="人生，是一场盛大的遇见"></p>
+    <p><img src="images/t-2.png" alt="若你懂得，就请珍惜。"></p>
+    <p><img src="images/t-3.png" alt="无论下多久的雨，最后都会有彩虹；无论你多么悲伤，要相信幸福在前方等候"></p>
+  </ul>
+</div>
+<div class="blank"></div>
+<div class="memorial_day">
+  <div class="time_axis"></div>
+  <ul>
+    <li class="n1"><a href="/">相遇</a>
+      <div class="dateview">2013-05-25</div>
+    </li>
+    <li class="n2"><a href="/">相识</a>
+      <div class="dateview">2013-06-01</div>
+    </li>
+    <li class="n3"><a href="/">网页生成</a>
+      <div class="dateview">2016-04-18</div>
+    </li>
+     <li class="n4"><a href="/">删除</a>
+      <div class="dateview">2016-05-29</div>
+    </li>
+
+  </ul>
+</div>
+<div class="blank"></div>
+<article>
+  <div class="l_box">
+    <div class="about_me">
+      <h2>关于我</h2>
+      <ul>
+        <img src="images/boy.jpg">
+        <p>网名：死神痛过</p>
+          <p>主页：<a href="http://www.sishen.ga" target="_blank">www.sishen.ga</a></p>
+        <p>职业：无职业</p>
+      </ul>
+    </div>
+    <div class="about_he">
+      <h2>关于她</h2><ul>
+        <img src="images/girl.jpg">
+        <p>网名：小2小2小小巫</p>
+        <p>主页：<br><a href="http://www.weiqing.cf/weiqing.html" target="_blank">weiqing.cf/weiqing.html</a></p>
+        <p>职业：业务</p>
+      </ul>
+      
+    </div>
+    <div class="newslist">
+      <h2>最新日志</h2>
+      <ul>
+        <li><a href="jilu-02.php" target="_blank">【死神】你无法体会我的孤独。</a></li>
+        <li><a href="jilu-03.php" target="_blank">如果这个城市只剩我和你</a></li>
+        <li><a href="jilu-04.php" target="_blank"> 这是一种什么样的体验</a></li>
+        <li><a href="jilu-05.php" target="_blank"> 你一直在向前走，从未回头</a></li>
+        <li><a href="jilu-06.php" target="_blank"> 我亦曾爱过你</a></li>
+        <li><a href="jilu-07.php" target="_blank"> 亲爱的，别留我一个人在这里</a></li>
+        <li><a href="/"> 无</a></li>
+        <li><a href="/"> 无</a></li>
+        <li><a href="/"> 无</a></li>
+ <!-- 备用 
+        <li><a href="/"> 爱情没有永远，地老天荒也走不完</a></li>
+        <li><a href="/"> 生活常有缺席的-可搞笑从来不缺席</a></li>
+        <li><a href="/"> 为了一个不存在的梦，执念了那么多年</a></li>
+        <li><a href="/"> 妹妹，明天你就要嫁人啦</a></li>
+        <li><a href="/"> 女孩都有浪漫的小情怀</a></li>
+        <li><a href="/"> 也许下个路口就会遇见希望</a></li>
+        <li><a href="/"> 6月毕业季，祝福送给你</a></li>
+  -->
+      </ul>
+    </div>
+    <div class="viny">
+      <ul>
+        <dl>
+          <dt class="art"><img src="images/artwork.png" alt="专辑"></dt>
+          <dd class="icon-song"><span></span>&#19971;&#31168;&#22346;</dd>
+          <dd class="icon-artist"><span></span>&#27468;&#25163;&#65306;&#26376;&#20043;&#38376;&#38899;&#20048;</dd>
+          <dd class="icon-album"><span></span>&#12298;&#21073;&#32593;3&#12299;&#28216;&#25103;&#38899;&#20048;</dd>
+          <dd class="music">
+              <audio src="http://jx.xcsee.cn/music/qixiufang.mp3" controls autoplay='autoplay'></audio>
+                <!--音乐名：七秀坊-->
+          </dd>
+          <!--也可以添加loop属性 音频加载到末尾时，会重新播放-->
+        </dl>
+      </ul>
+    </div>
+  </div>
+  <!--l_box end -->
+  <div class="r_box">
+    <li> <a href="jilu-07.php" target='_blank'><img src="images/03.jpg"></a>
+      <h3><a href="jilu-07.php" target='_blank'>亲爱的，别留我一个人在这里</a><font color=red>[置顶]</font></h3>
+      <p>就在我认为我快要习惯快要淡忘掉你有男朋友带给我的忧伤之际。就在我在空间说我要努力为以后努力的时候。你像是故意的，你不会不知道我有多难过。</p>
+    </li>
+      
+    <li> <a href="jilu-06.php" target='_blank'><img src="images/04.jpg"></a>
+      <h3><a href="jilu-06.php" target='_blank'>我亦曾爱过你</a></h3>
+      <p>我爱过你，你知道的。但是你说过，最笨的人才会把友情变为爱情,最后变为陌生人，所以我也就真的退却了。</p>
+    </li>
+      
+    <li> <a href="jilu-02.php" target="_blank"><img src="images/01.jpg"></a>
+        <h3><a href="jilu-02.php" target="_blank">【死神】你无法体会我的孤独。</a></h3>
+      <p>所有的事都是上天注定的。你和某一个人的关系如何，不是一两句话就可以改变的。即便可以改变些什么，已然已经疏远了。</p>
+    </li>
+      
+    <li> <a href="jilu-03.php" target="_blank"><img src="images/02.jpg"></a>
+      <h3><a href="jilu-03.php" target="_blank">如果这个城市只剩我和你</a></h3>
+      <p>我在网络上看到这样一句话，是说，所谓的成熟就是，曾经怎样努力都得不到的，现在不想要了。</p>
+    </li>
+      
+    <li> <a href="jilu-05.php" target='_blank'><img src="images/04.jpg"></a>
+      <h3><a href="jilu-05.php" target='_blank'>你一直在向前走，从未回头</a></h3>
+      <p>有些事，不是不在意，而是在意了又能怎样，人生没有如果，只有结果。你一直在努力向前走，从未回过头。</p>
+    </li>
+
+<!--
+    <li> <a href="/"><img src="images/03.jpg"></a>
+      <h3><a href="/">女孩都有浪漫的小情怀——浪漫的求婚词</a></h3>
+      <p>还在为浪漫的求婚词而烦恼不知道该怎么说吗？女孩子都有着浪漫的小情怀，对于求婚更是抱着满满的浪漫期待，也希望在求婚那一天对方可以给自己一个最浪漫的求婚词。</p>
+    </li>
+    <li> <a href="/"><img src="images/04.jpg"></a>
+      <h3><a href="/">擦肩而过</a></h3>
+      <p>《擦肩而过》文/清河鱼 编绘/天朝羽打开一扇窗，我不曾把你想得平常。看季节一一过往。你停留的那个地方，是否依然花儿开放？在夜里守靠着梦中的，想那仿佛前世铭刻进心肠的</p>
+    </li>
+    <li> <a href="/"><img src="images/01.jpg"></a>
+      <h3><a href="/">擦肩而过</a></h3>
+      <p>《擦肩而过》文/清河鱼 编绘/天朝羽打开一扇窗，我不曾把你想得平常。看季节一一过往。你停留的那个地方，是否依然花儿开放？在夜里守靠着梦中的，想那仿佛前世铭刻进心肠的</p>
+    </li>
+-->
+  </div>
+</article>
+    <div align=center>
+    <TABLE borderColor=#000000  height=30 width=100% cellPadding=1 width=100 align=center border=1>
+<TBODY>
+<TR>
+<TD>
+    &nbsp;友情链接：<a href="http://vip.xcsee.cn" target="_blank">死神影视</a>&nbsp;&nbsp;<a href="http://www.sishen.ga" target="_blank">死神主页</a>&nbsp;&nbsp;<a href="http://feixue.me" target="_blank">飞雪博客</a>&nbsp;&nbsp;<a href="http://vip.xcsee.cn/xingshi1.php" target="_blank">行尸走肉美剧</a>&nbsp;&nbsp;<a href="http://ys-wl.pw/" target="_blank">&#34945;&#24069;&#21338;&#23458;</a>
+     </TD></TR></TBODY></TABLE>
+    </div>
+<footer>
+  <p>Design by 刘坤</p>
+</footer>
+</body>
+</html>
