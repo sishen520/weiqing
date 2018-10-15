63f5931d9203526a5e3a18caa28164f6ce62fc01
sishen520 <34979795+sishen520@users.noreply.github.com>
2018-10-15 23:27:58 +0800

    Add files via upload

---
+++ v/default.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>默认页</title>
+    <meta charset="utf-8">
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta content="默认页" name="description">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Open Sans', 'Helvetica', sans-serif;
+            color: #000;
+            padding: 0;
+            margin: 0;
+            line-height: 1.428;
+        }
+        h1, h2, h3, h4, h5, h6, p {
+            padding: 0;
+            margin: 0;
+            color:#333333;
+        }
+        h1 {
+            font-size: 30px;
+            font-weight: 600!important;
+            color: #333;
+        }
+        h2 {
+            font-size: 24px;
+            font-weight: 600;
+        }
+        h3 {
+            font-size: 22px;
+            font-weight: 600;
+            line-height: 28px;
+        }
+        hr {
+            margin-top: 35px;
+            margin-bottom: 35px;
+            border: 0;
+            border-top: 1px solid #bfbebe;
+        }
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+        li {
+            display: inline-block;
+            float: right;
+            margin-left: 20px;
+            line-height: 35px;
+            font-weight: 100;
+        }
+        a {
+            text-decoration: none;
+            cursor: pointer;
+            -webkit-transition: all .3s ease-in-out;
+            -moz-transition: all .3s ease-in-out;
+            -ms-transition: all .3s ease-in-out;
+            -o-transition: all .3s ease-in-out;
+            transition: all .3s ease-in-out;
+        }
+        li a {
+            color: white;
+            margin-left: 3px;
+        }
+        li > i {
+            color: white;
+        }
+        .column-wrap a {
+            color: #5c34c2;
+            font-weight: 600;
+            font-size:16px;
+            line-height:24px;
+        }
+        .column-wrap p {
+            color: #717171;
+            font-size:16px;
+            line-height:24px;
+            font-weight:300;
+        }
+        .container {
+            margin-top: 100px;
+        }
+        .navbar {
+            position: relative;
+            min-height: 45px;
+            margin-bottom: 20px;
+            border: 1px solid transparent;
+        }
+        .navbar-brand {
+            float: left;
+            height: auto;
+            padding: 10px 10px;
+            font-size: 18px;
+            line-height: 20px;
+        }
+        .navbar-nav>li>a {
+            padding-top: 11px;
+            padding-bottom: 11px;
+            font-size: 13px;
+            padding-left: 5px;
+            padding-right: 5px;
+        }
+        .navbar-nav>li>a:hover {
+            text-decoration: none;
+            color: #cdc3ea!important;
+        }
+        .navbar-nav>li>a i {
+            margin-right: 5px;
+        }
+        .nav-bar img {
+            position: relative;
+            top: 3px;
+        }
+        .congratz {
+            margin: 0 auto;
+            text-align: center;
+        }
+        .message::before {
+            content: " ";
+            background: url(https://raw.githubusercontent.com/hostinger/banners/master/hostinger_welcome/images/hostinger-dragon.png);
+            width: 141px;
+            height: 175px;
+            position: absolute;
+            left: -150px;
+            top: 0;
+        }
+        .message {
+            width: 50%;
+            margin: 0 auto;
+            height: auto;
+            padding: 40px;
+            background-color: #eeecf9;
+            margin-bottom: 100px;
+            border-radius: 5px;
+            position:relative;
+        }
+        .message p {
+            font-weight: 300;
+            font-size: 16px;
+            line-height: 24px;
+        }
+        #pathName {
+            margin: 20px 10px;
+            color: grey;
+            font-weight: 300;
+            font-size:18px;
+            font-style: italic;
+        }
+        .column-custom {
+            border-radius: 5px;
+            background-color: #eeecf9;
+            padding: 35px;
+            margin-bottom: 20px;
+        }
+        .footer {
+            font-size: 13px;
+            color: gray !important;
+            margin-top: 25px;
+            line-height: 1.4;
+            margin-bottom: 45px;
+        }
+        .footer a {
+            cursor: pointer;
+            color: #646464 !important;
+            font-size: 12px;
+        }
+        .copyright {
+            color: #646464 !important;
+            font-size: 12px;
+        }
+        .navbar a {
+            color: white !important;
+        }
+        .navbar {
+            border-radius: 0px !important;
+        }
+        .navbar-inverse {
+            background-color: #434343;
+            border: none;
+        }
+        .column-custom-wrap{
+            padding-top: 10px 20px;
+        }
+        @media screen and (max-width: 768px) {
+            .message {
+                width: 75%;
+                padding: 35px;
+            }
+            .container {
+                margin-top: 30px;
+            }
+        }
+        }
+    </style>
+</head>
+
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button class="navbar-toggle" data-target="#myNavbar" data-toggle="collapse" type="button">
+                <span class="icon-bar"></span> <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="https://www.hostinger.com.hk/"><img src="https://raw.githubusercontent.com/hostinger/banners/master/hostinger_welcome/images/hostinger-logo.png" width="120"></a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="https://www.hostinger.com/tutorials"><i aria-hidden="true" class="fa fa-graduation-cap"></i> 教程</a>
+                </li>
+                <li>
+                    <a href="https://www.hostinger.com.hk/make-money-online"><i aria-hidden="true" class="fa fa-trophy"></i> 和我们一起赚钱</a>
+                </li>
+                <li>
+                    <a href="https://www.hostinger.com.hk/affiliate-program/login"><i aria-hidden="true" class="fa fa-user"></i> 推广</a>
+                </li>
+                <li>
+                    <a href="https://cpanel.hostinger.com.hk"><i aria-hidden="true" class="fa fa-lock"></i> 登录</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="empty-account-page">
+    <div class="container">
+        <div class="row congratz">
+            <h1>您的账号已创建!</h1><i></i>
+            <h2 id="pathName"><i></i></h2>
+        </div>
+        <div class="row message">
+            <p>网站<span id="website" style="word-break:break-all;"></span>已成功创建在服务器/伺服器上! 请从public_html目录中删除文件<span style="font-weight: bold;">default.php</span>,然后请通过FTP或是文件管理器来上传您的站点文件.</p>
+        </div>
+        <div class="col-xs-12">
+            <h2>接下来?</h2>
+        </div>
+        <div class="column-wrap clearfix">
+            <div class="col-xs-12">
+                <hr>
+            </div>
+            <div class="col-xs-12 col-sm-4 column-custom-wrap">
+                <div class="column-custom">
+                    <h3>精英网赚俱乐部</h3>
+                    <br>
+                    <p>在寻找最简单的赚钱方法吗? 加入Hostinger网赚俱乐部吧 - 我们全新的推广系统将让您在最短时间内获得利润.</p>
+                    <br>
+                    <a href="https://www.hostinger.com.hk/make-money-online">了解更多</a>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 column-custom-wrap">
+                <div class="column-custom">
+                    <h3 style="padding-bottom: 5px">知识库</h3>
+                    <br>
+                    <p>有想法建立一个网站却不知道从哪里开始吗? 所有问题的答案都在这里. 通过分类或是简单的输入单词来开始搜索吧.</p>
+                    <br>
+                    <a href="https://www.hostinger.com.hk/knowledge-base">了解更多</a>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 column-custom-wrap">
+                <div class="column-custom">
+                    <h3 style="padding-bottom: 5px">Hostinger博客</h3>
+                    <br>
+                    <p>加入Hostinger社区.订阅我们并接收Hostinger和业界的各种最新动态.</p>
+                    <br>
+                    <a href="https://www.hostinger.com/blog/">了解更多</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer">
+            <div class="row">
+                <div class="text-center" style="margin-bottom: 10px;">
+                    <a href="https://www.hostinger.com.hk/web-hosting">虚拟主机</a> |
+                    <a href="https://cpanel.hostinger.com.hk">客户登入</a>
+                </div>
+                <div class="copyright text-center">
+                    Hostinger © 2017. 保留所有权利
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    var pathName = window.location.hostname;
+    var account = document.getElementById("pathName");
+    var accountText = document.getElementById("website");
+    account.innerHTML = pathName;
+    accountText.innerHTML = pathName;
+</script>
+</body>
+
+</html>
